a447020a90f6cb643123eead20d89249780b5e55
Mowlid2341 <mohamedadan2341@gmail.com>
2022-06-30 11:45:34 -0700

    Done it

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main – entry point 
+*
+*Return: 0 aftre printing the function
+*/
+ Int main(void)
+
+{
+
+Puts (“\ “Programming is like building a multilingual puzzle”);
+
+return (0);
+
+}
+
+
