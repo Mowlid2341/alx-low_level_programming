3584fde062540f0ac1ea44b99d45f993e7054f96
Mowlid2341 <mohamedadan2341@gmail.com>
2022-07-02 04:43:13 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
 *main -task 1 print the last digit of the number store in the variable n
 *Return 0
-**/
+*/
 
 int main(void)
 {
@@ -18,15 +18,16 @@ n = rand() - RAND_MAX / 2;
 printf ("%s %d is %d and is", last,n,n % 10);
 if (n % 10 > 5)
 {
-printf ("greater than 5/n");
+printf("greater than 5/n");
 }
 else if (n % 10 == 0)
 {
-printf ("0\n");
+printf("0\n");
 }
 else 
 {
 printf("less than 6 and not o\n");
 }
 return (0);
+
 }
